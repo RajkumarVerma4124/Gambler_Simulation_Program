e6e4e12ff019676f7eb4b4e63e596292d79e6e99
RAJ <vermar4142@gmail.com>
2020-08-19 23:52:54 +0530

    Added:-Initialized The Stake,Bets And The Days

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
 echo "Welcome To The Program Of Gambling Simuation" 
 printf "\n"
-
+echo "As a Gambler, would start with a stake of $100 every day and bet $1 every game(UC1)."
+stakeMoney=100
+betMoney=1
+read -p "Enter The Number Of Days You Gonna Play : " days
