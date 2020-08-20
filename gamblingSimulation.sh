f7637fc15f946969cca6251a4f1fb2b09aef453a
RAJ <vermar4142@gmail.com>
2020-08-20 05:47:49 +0530

    Added:-The Functionality For Resign If Won Or Lose 50 Percent

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,20 +1,36 @@
-#!/bin/bash -x
 echo "Welcome To The Program Of Gambling Simuation" 
 printf "\n"
-#As a Gambler, would start with a stake of $100 every day and bet $1 every game(UC1)."
+#constants
 stakeMoney=100
 betMoney=1
 read -p "Enter The Number Of Days You Gonna Play : " days
 printf "\n"
-#As a Gambler make $1 bet so either win or loose $1(UC2)"
 function checkBet() {
-	if [ $(($RANDOM % 2)) -eq 1 ]
-	then
-		echo "You Won The Bet "
-		stakeMoney=$(($stakeMoney + $betMoney))
-	else
-		echo "You Lose The Bet "
-		stakeMoney=$(($stakeMoney - $betMoney))
-	fi
+	#As a Calculative Gambler if won or lost 50% of the stake, would resign for the day(UC3)"
+	while [ $days -gt 0 ]
+	do	
+		#As a Gambler, starting with a stake of $100 every day and bet $1 every game(UC1)."
+		#variables
+		stakeMoney=100				
+		betMoney=1
+		
+		dailyStake=$stakeMoney;
+		highStake=$((stakeMoney + stakeMoney/2))
+		lowStake=$((stakeMoney - stakeMoney/2))
+		while [ $stakeMoney -gt $((lowStake)) ] && [ $stakeMoney -lt $((highStake)) ]
+		do	
+			#As a Gambler making $1 bet so either win or loose $1(UC2)"
+			if [ $(($RANDOM % 2)) -eq 1 ]
+			then
+				echo "You Won The Bet "
+				stakeMoney=$(($stakeMoney + $betMoney))
+			else
+				echo "You Lose The Bet "
+				stakeMoney=$(($stakeMoney - $betMoney))
+			fi
+		done
+	echo "You Have Resign For The Day With The Stake Of : "$stakeMoney
+	days=$(($days-1))
+	done
 }
 checkBet
