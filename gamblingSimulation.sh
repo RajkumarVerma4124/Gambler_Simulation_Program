13e0407562cd0c69afc16ddd181a17e46deb44cd
RAJ <vermar4142@gmail.com>
2020-08-20 05:54:07 +0530

    Added:-The Functionality To Get The Total Win Or Lose After 20 Days

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -9,28 +9,36 @@ function checkBet() {
 	#As a Calculative Gambler if won or lost 50% of the stake, would resign for the day(UC3)"
 	while [ $days -gt 0 ]
 	do	
-		#As a Gambler, starting with a stake of $100 every day and bet $1 every game(UC1)."
-		#variables
 		stakeMoney=100				
 		betMoney=1
-		
 		dailyStake=$stakeMoney;
+		totalStake=$(($totalStake+$dailyStake))
 		highStake=$((stakeMoney + stakeMoney/2))
 		lowStake=$((stakeMoney - stakeMoney/2))
 		while [ $stakeMoney -gt $((lowStake)) ] && [ $stakeMoney -lt $((highStake)) ]
 		do	
-			#As a Gambler making $1 bet so either win or loose $1(UC2)"
 			if [ $(($RANDOM % 2)) -eq 1 ]
 			then
-				echo "You Won The Bet "
 				stakeMoney=$(($stakeMoney + $betMoney))
 			else
-				echo "You Lose The Bet "
 				stakeMoney=$(($stakeMoney - $betMoney))
 			fi
 		done
-	echo "You Have Resign For The Day With The Stake Of : "$stakeMoney
+	if [ $stakeMoney -eq 50 ]
+	then
+		amountLose=$(($amountLose+50))
+	else
+		amountWin=$(($amountWin+50))
+	fi
 	days=$(($days-1))
 	done
+
+	#After 20 days of playing every day Shwoing the total amount won or lost(UC4)."
+	totalAmount=$(($totalStake+($amountWin-$amountLose)))
+	echo "Total Amount Spend On The Bets : $totalStake"
+	echo "You Won The Total Amount Of : $amountWin"
+	echo "You Lose The Total Amount Of : $amountLose"
+	echo "After 20 Days You Have Total Amount Of : $totalAmount" 
 }
+
 checkBet
