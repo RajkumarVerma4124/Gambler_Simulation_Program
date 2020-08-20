5808c24389623ab9edb66f11406de8e70612fd93
RAJ <vermar4142@gmail.com>
2020-08-20 06:05:55 +0530

    Added:-The Functionality To Check Each Month Days Won Or Lost And By How Much

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,22 +1,27 @@
 echo "Welcome To The Program Of Gambling Simuation" 
 printf "\n"
-#constants
-stakeMoney=100
-betMoney=1
-read -p "Enter The Number Of Days You Gonna Play : " days
-printf "\n"
 function checkBet() {
-	#As a Calculative Gambler if won or lost 50% of the stake, would resign for the day(UC3)"
-	while [ $days -gt 0 ]
-	do	
+	#Each month Showing the days won and lost and by how much(UC5)."
+	#variables
+	i=0
+	amountLose=0
+	amountWin=0
+	read -p "Enter The Number Of Days You Gonna Play : " days
+	while [ $days -gt $i ]
+	do
+		#As a Gambler, would start with a stake of $ 100 every day and bet $1 every game(UC1).
+		#constants
 		stakeMoney=100				
-		betMoney=1
-		dailyStake=$stakeMoney;
-		totalStake=$(($totalStake+$dailyStake))
+		betMoney=1	
+
+		totalStake=$(($totalStake+$stakeMoney))
 		highStake=$((stakeMoney + stakeMoney/2))
 		lowStake=$((stakeMoney - stakeMoney/2))
+
+		#As a Calculative Gambler if won or lost 50% of the stake, would resign for the day(UC3)"
 		while [ $stakeMoney -gt $((lowStake)) ] && [ $stakeMoney -lt $((highStake)) ]
-		do	
+		do
+			#As a Gambler making $1 bet so either win or loose $1(UC2)	
 			if [ $(($RANDOM % 2)) -eq 1 ]
 			then
 				stakeMoney=$(($stakeMoney + $betMoney))
@@ -24,21 +29,28 @@ function checkBet() {
 				stakeMoney=$(($stakeMoney - $betMoney))
 			fi
 		done
-	if [ $stakeMoney -eq 50 ]
-	then
-		amountLose=$(($amountLose+50))
-	else
-		amountWin=$(($amountWin+50))
-	fi
-	days=$(($days-1))
-	done
+		i=$(($i+1))
 
-	#After 20 days of playing every day Shwoing the total amount won or lost(UC4)."
+		if [ $stakeMoney -eq 50 ]
+		then
+			amountLose=$(($amountWin+50))
+			daysLose=$(($daysLose+1))
+			echo "Day : $i " "Amount Lose : $amountLose"
+		else
+			amountWin=$(($amountWin+50))
+			daysWon=$(($daysWon+1))
+			echo "Day : $i " "Amount Won : $amountWin"
+		fi
+	
+	done
+	printf "\n"
+	#After no. Of days of playing Showing the total amount won or lost(UC4)."
 	totalAmount=$(($totalStake+($amountWin-$amountLose)))
 	echo "Total Amount Spend On The Bets : $totalStake"
 	echo "You Won The Total Amount Of : $amountWin"
 	echo "You Lose The Total Amount Of : $amountLose"
-	echo "After 20 Days You Have Total Amount Of : $totalAmount" 
+	echo "Total No. Of Days Won : $daysWon"
+	echo "Total No. Of Days Lose : $daysLose"
+	echo "After $i Days You Have Total Amount Of : $totalAmount" 
 }
-
 checkBet
